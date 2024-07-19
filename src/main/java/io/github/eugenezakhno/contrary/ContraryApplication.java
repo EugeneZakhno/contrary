784c0b92e3381d922fedf043d0b7308ee198c292
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-07-19 17:01:30 +0300

    initt

---
+++ src/main/java/io/github/eugenezakhno/contrary/ContraryApplication.java
@@ -0,0 +1,13 @@
+package io.github.eugenezakhno.contrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/eugenezakhno/contrary/ContraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eugenezakhno.contrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
